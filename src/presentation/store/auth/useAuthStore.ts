88601f2c8305e7b23488a76693ba507a44e99dfe
Pedro Cajas <45060148+pbooox@users.noreply.github.com>
2024-11-01 13:44:38 -0600

    preparando gestor de estado con zustand

---
+++ src/presentation/store/auth/useAuthStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { User } from "../../../domain/entities/user";
+import { AuthStatus } from "../../../infrastructure/interfaces/auth.status";
+import { authLogin } from "../../../actions/auth/auth";
+
+// creamos la interface para el estado
+export interface AuthState {
+    status: AuthStatus,
+    token?: string;
+    user?: User;
+
+    // funcion de login que hicimos en /actions/auth/auth.ts
+    login: (email: string, password: string) => Promise<Boolean>;
+}
+
+export const useAuthStore =  create<AuthState>()( (set, get) => ({
+    status: 'checking',
+    token: undefined,
+    user: undefined,
+
+    login: async (email: string, password: string) => {
+        const resp = await authLogin(email, password);
+        if( !resp ) {
+            set({ status: 'unauthenticated', token: undefined, user: undefined });
+            return false;
+        }
+
+        // TODO: Guardar el token  y usuario en el storage del dispositivo
+
+        set({ status: 'authenticated', token: resp.token, user: resp.user })
+        return true;
+    }
+}))
