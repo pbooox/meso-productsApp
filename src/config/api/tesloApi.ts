8c617fdfc5ddd7ec4f83ff1c6f3407b9a395c418
Pedro Cajas <45060148+pbooox@users.noreply.github.com>
2024-11-01 16:15:12 -0600

    verificar el estado de la autenticacion

--- src/config/api/tesloApi.ts
+++ src/config/api/tesloApi.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { API_URL } from '@env';
+import { StorageAdapter } from '../adapters/storage-adapter';
 
 // creamos el cliente que usaremos para poder hacer las peticiones
 
@@ -12,6 +13,15 @@ const tesloApi = axios.create({
 
 // TODO: Interceptors
 // los interceptors nos van a ayudar a leer de nuestro storage fisico y poder adjuntar nuestro token de acceso
+tesloApi.interceptors.request.use(
+    async (config) => {
+        const token = await StorageAdapter.getItem('token');
+        if( token ) {
+            config.headers['Authorization'] = `Bearer ${token}`
+        } 
+        return config
+    }
+);
 
 export {
     tesloApi,
--- src/presentation/store/auth/useAuthStore.ts
+++ src/presentation/store/auth/useAuthStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { User } from "../../../domain/entities/user";
 import { AuthStatus } from "../../../infrastructure/interfaces/auth.status";
-import { authLogin } from "../../../actions/auth/auth";
+import { authCheckStatus, authLogin } from "../../../actions/auth/auth";
 import { StorageAdapter } from "../../../config/adapters/storage-adapter";
 
 // creamos la interface para el estado
@@ -12,6 +12,7 @@ export interface AuthState {
 
     // funcion de login que hicimos en /actions/auth/auth.ts
     login: (email: string, password: string) => Promise<Boolean>;
+    checkStatus: () => Promise<void>
 }
 
 export const useAuthStore =  create<AuthState>()( (set, get) => ({
@@ -25,12 +26,19 @@ export const useAuthStore =  create<AuthState>()( (set, get) => ({
             set({ status: 'unauthenticated', token: undefined, user: undefined });
             return false;
         }
-
         // TODO: Guardar el token  y usuario en el storage del dispositivo
         await StorageAdapter.setItem('token', resp.token)
-
-
         set({ status: 'authenticated', token: resp.token, user: resp.user })
         return true;
+    },
+    checkStatus: async () => {
+        const resp = await authCheckStatus();
+        if( !resp ) {
+            set({ status: 'unauthenticated', token: undefined, user: undefined });
+            return;
+        }
+        await StorageAdapter.setItem('token', resp.token)
+        set({ status: 'authenticated', token: resp.token, user: resp.user })
+        return;
     }
 }))
