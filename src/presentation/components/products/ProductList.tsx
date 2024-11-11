94c8d92f4dbfb3fc4f62c084e54bc198c7f25b27
Pedro Cajas <45060148+pbooox@users.noreply.github.com>
2024-11-10 22:30:06 -0600

    agregamos pull to refresh

--- src/presentation/components/products/ProductList.tsx
+++ src/presentation/components/products/ProductList.tsx
@@ -1,6 +1,8 @@
-import { Layout, List, ListItem, Text } from "@ui-kitten/components"
+import { Layout, List } from "@ui-kitten/components"
 import { Product } from "../../../domain/entities/product"
 import { ProductCard } from "./ProductCard"
+import { useState } from "react";
+import { RefreshControl } from "react-native";
 
 interface Props {
   products: Product[];
@@ -8,6 +10,15 @@ interface Props {
 }
 
 export const ProductList = ({ products, fetchNextPage }: Props) => {
+
+  const [IsRefreshing, setIsRefreshing] = useState(false);
+
+  const onPullToRefresh = async () => {
+    setIsRefreshing(true);
+    // sleep 1.5 segundos
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    setIsRefreshing(false);
+  }
 
   const renderItem = ({ item }: any): React.ReactElement => (
     <ProductCard product={item} />
@@ -22,6 +33,13 @@ export const ProductList = ({ products, fetchNextPage }: Props) => {
       ListFooterComponent={() => <Layout style={{ height: 150 }} />}
       onEndReached={fetchNextPage}
       onEndReachedThreshold={0.5}
+
+      refreshControl={
+        <RefreshControl
+          refreshing={IsRefreshing}
+          onRefresh={onPullToRefresh}
+        />
+      }
 
     />
   )
