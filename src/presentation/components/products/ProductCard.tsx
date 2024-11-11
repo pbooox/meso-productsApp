9cb99025a6be90f86cfe8f35475b3831bade1974
Pedro Cajas <45060148+pbooox@users.noreply.github.com>
2024-11-10 23:44:39 -0600

    pantalla de producto

--- src/presentation/components/products/ProductCard.tsx
+++ src/presentation/components/products/ProductCard.tsx
@@ -2,15 +2,21 @@ import { Card, Text } from "@ui-kitten/components"
 import { Product } from "../../../domain/entities/product"
 import { Image } from "react-native"
 import { FadeInImage } from "../ui/FadeInImage"
+import { NavigationProp, useNavigation } from "@react-navigation/native"
+import { RootStackParams } from "../../navigation/StackNavigator"
 
 interface Props {
   product: Product
 }
 
 export const ProductCard = ({ product }: Props) => {
+
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
   return (
     <Card
       style={{ flex: 1, backgroundColor: '#F9F9F9', margin: 3 }}
+      onPress={() => navigation.navigate('ProductScreen', { productId: product.id })}
     >
       {
         (product.images.length === 0)
--- src/presentation/screens/product/ProductScreen.tsx
+++ src/presentation/screens/product/ProductScreen.tsx
@@ -1,10 +1,36 @@
-import { Text, View } from 'react-native'
+import { StackScreenProps } from '@react-navigation/stack'
+import { RootStackParams } from '../../navigation/StackNavigator'
+import { useQuery } from '@tanstack/react-query';
+import { getProductById } from '../../../actions/products/get-product-by-id';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Text } from '@ui-kitten/components';
+import { useRef } from 'react';
 
-export const ProductScreen = () => {
-    return (
-      <View>
-        <Text>ProductScreen</Text>
-      </View>
-    )
+interface Props extends StackScreenProps<RootStackParams, 'ProductScreen'> { }
+
+export const ProductScreen = ({ route }: Props) => {
+
+  const productIdRef = useRef(route.params.productId);
+  // useQuery
+  const { data: product } = useQuery({
+    queryKey: ['product', productIdRef.current],
+    queryFn: () => getProductById(productIdRef.current),
+  });
+
+  // useMutation, para hacer la actualizacion del producto
+
+  // si aun no encuentra producto mostrara el titulo de cargando
+  if (!product) {
+    return (<MainLayout title="Cargando..." />)
+  }
+
+  return (
+    <MainLayout
+      title={product.title}
+      subTitle={`Precio:${product.price}`}
+    >
+      <Text>Hola mundo</Text>
+    </MainLayout>
+  )
 }
 
